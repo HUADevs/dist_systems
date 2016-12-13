1f1c82309fd7cfb065fb2d5a04813bc0c1b006fa
Marios Alexiou <it21405@hua.gr>
2016-12-13 14:24:48 +0200

    created servicescontroller

--- Code/src/main/java/com/huaDevelopers/controllers/ServicesController.java
+++ Code/src/main/java/com/huaDevelopers/controllers/ServicesController.java
@@ -1,16 +1,40 @@
 package com.huaDevelopers.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.huaDevelopers.data.Entities.Customer;
+import com.huaDevelopers.data.Entities.Services;
+import com.huaDevelopers.data.Services.ServEntityService;
+
+@Controller
+@RequestMapping("/cms/admin/services")
 public class ServicesController {
 
-	public ServicesController() {
-		// TODO Auto-generated constructor stub
+	private ServEntityService s_service;
+	
+	@Autowired
+	@Qualifier(value = "s_service")
+	public void setServEntityService (ServEntityService s_service){
+		this.s_service=s_service;
 	}
 	
-	public void addNewService() {
-		// TODO - implement ServicesController.addNewService
-		throw new UnsupportedOperationException();
+	@RequestMapping(value="/add" , method = RequestMethod.GET)
+	public String addService(Model model) {
+		model.addAttribute("service", new Services());
+		return "addService";
 	}
-
+	
+	@RequestMapping(value="/add" , method= RequestMethod.POST)
+	public String saveService(@ModelAttribute("service") Services serv){
+		
+		return "";	
+	}
 	public void RemoveService() {
 		// TODO - implement ServicesController.RemoveService
 		throw new UnsupportedOperationException();
